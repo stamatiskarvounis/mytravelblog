74c2f5a3e7790c3c97ca0421af256b356fbe5597
stamatiskarvounis <stamatiskarvounis@hotmail.com>
2020-04-20 16:42:28 +0100

    Creation of Blog App, initial attempt

--- mytravelblog/settings.py
+++ mytravelblog/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'homepage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'mytravelblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mytravelblogdb',
+        'USER':'postgres',
+        'PASSWORD': 'Aa6942862699',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -118,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL="/media/"
--- mytravelblog/urls.py
+++ mytravelblog/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+import homepage.views
+from django.conf.urls import include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', homepage.views.homepage, name="homepage"),
+    path('blog/', include('blog.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
